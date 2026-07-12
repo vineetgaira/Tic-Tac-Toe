4512c8b29ec3740cc6f38f7b3c174a8b56d7b58c
Vineet Gaira <vineetgaira1729@gmail.com>
2026-07-12 11:00:02 +0530

    Fix all the bugs in game.py

--- game.py
+++ game.py
@@ -1,13 +1,11 @@
 from constants import BOARD_SIZE
 
-def play_game():
-    pass
-
 def check_rows(board,player):
 # This checks every row, god damn it
     for i in range(BOARD_SIZE):
         if all(board[i][j]==player for j in range(BOARD_SIZE)):
             return True
+    return False
 
 
 def check_col(board,player):
@@ -15,12 +13,13 @@ def check_col(board,player):
     for i in range(BOARD_SIZE):
         if all(board[j][i]==player for j in range(BOARD_SIZE)):
             return True
+    return False
     
 def check_diagonal(board,player):
 # Now I have to make a function to check every diagonal.
     if board[0][0] == board[1][1] == board[2][2] == player:
         return True
-    if board[2][0] == board[2][2] == board[0][2] == player:
+    if board[2][0] == board[1][1] == board[0][2] == player:
         return True
     
     return False
@@ -29,6 +28,7 @@ def check_winner(board,player):
 # This will produce a winner
     if check_rows(board,player) or check_col(board,player) or check_diagonal(board,player):
         return True
+    return False
         
     
 def check_draw(board):
@@ -36,7 +36,7 @@ def check_draw(board):
 # This will check the draw I still don't know how to write these by my own.
    return all(col != " " for row in board for col in row)
 
-def switch_user(player):
+def switch_player(player):
 # This will switch users
     if player=="X":
         player="O"
