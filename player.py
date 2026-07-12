fba9abea35f902f99127fa7b59677b1ad33b764a
Vineet Gaira <vineetgaira1729@gmail.com>
2026-07-12 09:26:57 +0530

    Recheck get player move

--- player.py
+++ player.py
@@ -1,25 +1,31 @@
+import colorama
+from colorama import Fore
 from board import create_board
+colorama.init(autoreset=True)
 
 def get_player_move(board):
     while True:
         try:
-            move=input("Please enter a row and column (row, column) (eg.,0 2):").split()
+            move=input(Fore.BLUE+"Please enter a row and column (row, column) (eg.,0 2):").split()
             
             if len(move)!=2:
-                print("Please enter exactly two numbers!")
+                print(Fore.RED+"Please enter exactly two numbers!")
                 continue
 
             row, col = int(move[0]), int(move[1])
 
             if row not in range(3) or col not in range(3):
-                print("Please enter a valid number!")
+                print(Fore.RED+"Please enter a valid number!")
                 continue
             
             if board[row][col] != " ":
-                print("That place is already taken.")
+                print(Fore.RED+"That place is already taken.")
                 continue
 
         except ValueError:
-            print("Please enter a valid integer.")
+            print(Fore.RED+"Please enter a valid integer.")
             continue
+        break
 
+board=create_board()
+get_player_move(board)
