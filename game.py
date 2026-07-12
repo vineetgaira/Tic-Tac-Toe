4cca3f2a6d57287901cd260b60dfcb021045ecb2
Vineet Gaira <vineetgaira1729@gmail.com>
2026-07-12 09:42:57 +0530

    check rows

--- game.py
+++ game.py
@@ -1,4 +1,19 @@
+from constants import BOARD_SIZE
+
 def play_game():
+    pass
+
+def check_rows(board,player):
+
+    for i in range(BOARD_SIZE):
+        if all(board[i][j]==player for j in range(BOARD_SIZE)):
+            return True
+
+
+def check_col(board,player):
+    pass
+
+def check_diagonal():
     pass
 
 def check_winner():
